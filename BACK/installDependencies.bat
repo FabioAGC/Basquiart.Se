d30afbddd9bcdf3c29bd5bd77247d76edea88451
FabioAGC <dev.fabio.cruvinel@gmail.com>
2026-03-17 17:46:40 -0300

    primeira versão

---
+++ BACK/installDependencies.bat
@@ -0,0 +1,11 @@
+@echo off
+
+docker compose -f database/docker-compose.yml up -d --force-recreate
+
+if not exist venv (
+    python -m venv venv
+)
+call venv\Scripts\activate
+pip install -r requirements.txt
+
+pause
